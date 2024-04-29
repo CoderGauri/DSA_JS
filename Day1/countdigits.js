05a797fde01acb864a220460159a77d084e84fea
Gauri <gauriM304@gmail.com>
2024-04-29 11:22:47 +0530

    Problem 1

---
+++ Day1/countdigits.js
@@ -0,0 +1,28 @@
+let digit = 153;
+
+//solution 1
+// function countDigits(digit) {
+//   let count = 0;
+//   while (digit > 0) {
+//     digit = Math.floor(digit / 10);
+//     count++;
+//   }
+//   return count;
+// }
+
+//console.log(countDigits(digit));
+
+//solution 2
+// function countDigits1(digit)
+// {
+// let count = 0;
+// while(digit > 0){
+//     //lastdigit = digit % 10;
+//     count = count + 1;
+//     digit = digit/10
+//     console.log(digit);
+// }
+// return count
+// }
+
+// console.log(countDigits1(digit));
