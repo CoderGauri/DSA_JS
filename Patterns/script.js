fb81b029d0dc05adf1a7e9e0c09eb457c6c3afb6
Gauri <gauriM304@gmail.com>
2024-05-27 20:26:31 +0530

    Pattern 1

--- Patterns/script.js
+++ Patterns/script.js
@@ -4,3 +4,35 @@ inner loop - focus on the column and the connection to the rows
 print the "pattern" inside the inner loop
 observe symmetry
 */
+
+/*Pattern 1
+* * *
+* * *
+* * **/
+
+for (let i = 0; i < 10; i++) {
+  for
+   (let j = 0; j < 4; j++) {}
+  //console.log("*");
+}
+//solution 1
+function printPattern(rows) {
+    for (let i = 0; i < rows; i++) {
+        let line = "";
+        for (let j = 0; j < 3; j++) {
+            line += "* * *\n";
+        }
+        console.log(line);
+    }
+}
+
+printPattern(1);
+//solution 2
+for (let i = 0; i < 3; i++) {
+    let row = "";
+    for (let j = 0; j < 3; j++) {
+        row += "* ";
+    }
+    console.log(row);
+}
+
